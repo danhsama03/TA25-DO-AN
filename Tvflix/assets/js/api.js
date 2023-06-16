e65149b686bd9041ba37c4ada288bb17cbd4c628
danhsama03 <phamcongdanh2003@gmail.com>
2023-06-16 20:04:30 +0700

    Update chinh sua api.js

--- Tvflix/assets/js/api.js
+++ Tvflix/assets/js/api.js
@@ -3,16 +3,22 @@
 const api_key = '3fd67b0a75a2861ff71511c8065512a7';
 const imageBaseURL = 'https://image.tmdb.org/t/p/';
 
-/**
- * fetch data from a server using the `url` and passes
- * the result in JSON data to a `callback` function,
- * along with an optional parameter if has `optionalParam`.
- */
+// const fetchDataFromServer = function (url, callback, optionalParam) {
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(data => callback(data, optionalParam));
+// }
 
-const fetchDataFromServer = function (url, callback, optionalParam) {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => callback(data, optionalParam));
-}
+const fetchDataFromServer = async function (url, callback, optionalParam) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    callback(data, optionalParam);
+  } catch (error) {
+    // Xử lý lỗi nếu có
+    console.error('Lỗi khi lấy dữ liệu từ máy chủ:', error);
+  }
+};
+
 
 export { imageBaseURL, api_key, fetchDataFromServer };
